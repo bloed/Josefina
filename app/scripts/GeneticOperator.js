d6d1c86b0e5f8cefc08093f11187a3121e966cf5
Adrian Lopez 2 <adrianlq16@hotmail.com>
2015-05-16 18:07:31 -0600

    crossing, mutation y cromosomatica
    falta un metodo de obtener el string para l nuevo individuo

--- app/scripts/GeneticOperator.js
+++ app/scripts/GeneticOperator.js
@@ -2,11 +2,26 @@ var GeneticOperator= Class.extend({
     init: function(){
         
     },
-    mutation: function(pIndividual, pPorcentage){
+    mutation: function(pIndividual){
         //not of a bit
+        var mutationNumber = Math.floor(Math.random()*100);
+        if(mutationNumber<MUTATION_PROBABILITY){   //it exists in th minority
+        	var mutationPoint=Math.floor(Math.random()*(AMOUNT_OF_BITS-1));
+        	var bitMask= 1<<mutationPoint;
+        	pIndividual=pIndividual^bitMask;
+        }
+        return pIndividual;
     },
     cross:function (pFather, pMother){
         //variable point
+        var crossPoint=Math.floor((Math.random()*(AMOUNT_OF_BITS-1))+1);
+        alert(crossPoint);
+        pFather=pFather>>>(AMOUNT_OF_BITS-crossPoint);
+        pFather= pFather<<(AMOUNT_OF_BITS-crossPoint);
+        var motherMask=pMother>>>(AMOUNT_OF_BITS-crossPoint);
+        motherMask=motherMask<<(AMOUNT_OF_BITS-crossPoint);
+        pMother=pMother^motherMask;
+        this.mutation(pFather+pMother);
     }
 });
 
--- app/scripts/IndividualRepresentation.js
+++ app/scripts/IndividualRepresentation.js
@@ -4,8 +4,32 @@ var IndividualRepresentation= Class.extend({
         //{ word: '', minValue: 0, maxValue: 0 }
     },
     calculateChromosomaticRepresentation: function(pListOfIndividuals){
-         
-    }
+         var totalValues=0;
+         var arrayLength = pListOfIndividuals.length;
+		 for (var index = 0; index < arrayLength; index++) {
+		 	totalValues+=pListOfIndividuals[index].weigth+pListOfIndividuals[index].distance;
+		 }
+		 var minRange=0;
+		 var maxRange=0;
+		 var bitsMaxNumber=Math.pow(2, AMOUNT_OF_BITS)-1;
+		 for(var index =0; index <arrayLength-1; index++){
+		 	maxRange=Math.floor((pListOfIndividuals[index].weigth+pListOfIndividuals[index].distance)*bitsMaxNumber/totalValues);
+		 	this._ChromosomaticRepresentation.push( {word: pListOfIndividuals[index].word, minValue: minRange,
+		 	maxValue: (minRange+maxRange)})
+		 	minRange+=maxRange+1;
+		 }
+
+		  	this._ChromosomaticRepresentation.push( {word: pListOfIndividuals[pListOfIndividuals.length-1].word, 
+		  		minValue: minRange, maxValue: bitsMaxNumber})
+
+		 /*for(var index=0; index<this._ChromosomaticRepresentation.length;index++){
+		 	alert(this._ChromosomaticRepresentation[index].word + this._ChromosomaticRepresentation[index].minValue + "-" +
+		 		this._ChromosomaticRepresentation[index].maxValue);
+		 }  PRINT DE VISUALIZACION, SE QUITA */
+	},
+	getWordString: function(){
+		//return the string of the new individual
+	}
 });
 
 
