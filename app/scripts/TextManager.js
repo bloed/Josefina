6b0122ab250dbadc0564448dc293f8fee64e78b3
bloed <josuearrietasalas@gmail.com>
2015-05-23 17:15:34 -0600

    nuevo "calculo" de grafo

--- app/scripts/TextManager.js
+++ app/scripts/TextManager.js
@@ -108,26 +108,62 @@ var TextManager = Class.extend({
         }
         return totalOfAppearances;
     },
-    calculateWeight: function(pWord,pDistance){
+    calculateAllWeight: function(pListOfWords,listPercentage){
         var weight = 0;
-        var percentageOfDistance = Math.round((pDistance/this._TotalDifferentDistance)*100);
-
-        
-        if (weight===0){
-             weight=1;
+        var percentageOfDistance =0;
+        var currentPercentage;
+        var minRange;
+        var maxRange;
+        var listOfIndividuals = [];
+        var individual;
+        var controlOwnRelation = false;
+        for(var indexArrayDistance = 0;  indexArrayDistance < pListOfWords.length ; indexArrayDistance++){
+            percentageOfDistance = Math.round((pListOfWords[indexArrayDistance].distance/this._TotalDifferentDistance)*100);
+            minRange = Math.floor((percentageOfDistance/10)*10);
+            maxRange = minRange + 9;//we are grouping by centenas of percentage;
+            controlOwnRelation = false;
+            weight=0;
+            for(var indexArrayPercentage = 0; indexArrayPercentage < listPercentage.length ; indexArrayPercentage++){
+                currentPercentage = listPercentage[indexArrayPercentage];
+                if(currentPercentage>= minRange &&currentPercentage<=maxRange){
+                    if(percentageOfDistance === currentPercentage){//dangerous case
+                        if(controlOwnRelation === true){
+                            weight++;
+                        }else{
+                            controlOwnRelation = true;//the next time it will be related
+                        }
+                    }else{
+                        weight++;
+                    }
+                }
+            }
+            var individual = new Individual(weight,pListOfWords[indexArrayDistance].distance,0,pListOfWords[indexArrayDistance].word,0);
+            listOfIndividuals.push(individual);
         }
-        return weight;
+        return listOfIndividuals;
     },
-    calculateDistance : function (pWord){
-
-        var totalOfAppeareances = 0;
-        
-         /*for(var indexOfArray = 0 ; indexOfArray < this._ListOfWords.length; indexOfArray++){
-             if (this._ListOfWords[indexOfArray] === pWord){
-                 totalOfAppeareances++;
-             }
-         }
-         return totalOfAppeareances;*/
+    calculateAllDistances : function (){
+        var listOfDistances = [];
+        var wordFound;
+        for(var indexOfArray = 0 ; indexOfArray < this._ListOfWords.length; indexOfArray++){
+            wordFound = false;
+            for(var indexOfDistance = 0 ;indexOfDistance < listOfDistances.length; indexOfDistance++){
+                if (this._ListOfWords[indexOfArray] === listOfDistances[indexOfDistance].word){
+                    listOfDistances[indexOfDistance].distance++;
+                    wordFound=true;
+                    break;
+                }
+            }
+            if(wordFound === false){
+               listOfDistances.push({word :this._ListOfWords[indexOfArray] ,  distance : 1}); 
+            }
+        }
+        var result = "";
+        for(var indexOfDistance = 0 ;indexOfDistance < listOfDistances.length; indexOfDistance++){
+                result+= listOfDistances[indexOfDistance].word + " distancia : " + listOfDistances[indexOfDistance].distance  + " ";
+            }
+        alert(result);
+        return listOfDistances;
     },
     /*calculateAverageOfDistances : function(){
 
@@ -144,31 +180,58 @@ var TextManager = Class.extend({
         return Math.floor(totalDistance/numberOfWords);
     }//Nuevas funciones
     ,*/
-    calculateAllDistances : function(pListOfDistances){
+    calculateMaxDistances : function(pListOfDistances){
         var processedDistances = [];
         var totalDistance = 0;
         for(var indexArray = 0; indexArray < pListOfDistances.length ; indexArray++){
-            if(processedDistances.indexOf(pListOfDistances[indexArray])===-1){
-                totalDistance += pListOfDistances[indexArray];
-                processedDistances.push(pListOfDistances[indexArray]);
+            if(processedDistances.indexOf(pListOfDistances[indexArray].distance)===-1){
+                totalDistance += pListOfDistances[indexArray].distance;
+                processedDistances.push(pListOfDistances[indexArray].distance);
             }
         }
         this._TotalDifferentDistance = totalDistance;
-        return totalDistance;
+        alert("distancia diferente total : " + this._TotalDifferentDistance);
+        //return totalDistance;
     },
     calculatePercentagePerDistance : function(pListOfDistances){
         //sets a new array of the form [distance : , percentage : ], sorted by percentage
         var listPercentagePerDistance = [];
-        var processedDistances = [];
         var percentageOfDistance = 0;
         for(var indexArray = 0; indexArray < pListOfDistances.length ; indexArray++){
-            if(processedDistances.indexOf(pListOfDistances[indexArray])===-1){
-                percentageOfDistance = Math.round((pListOfDistances[indexArray]/this._TotalDifferentDistance)*100);
-                listPercentagePerDistance.push(percentageOfDistance);
-            }
+            percentageOfDistance = Math.round((pListOfDistances[indexArray].distance/this._TotalDifferentDistance)*100);
+            listPercentagePerDistance.push(percentageOfDistance);
+        } 
+        listPercentagePerDistance.sort();
+        var result = "";
+        for(var caca = 0 ;  caca<listPercentagePerDistance.length ; caca++){
+            result+= "-" + listPercentagePerDistance[caca] + " - ";
         }
-        listPercentagePerDistance.sort();        
-        this._ListOfPercentagePerDistance = listPercentagePerDistance;
+        alert(result);
+        //this._ListOfPercentagePerDistance = listPercentagePerDistance;
+        return listPercentagePerDistance;
+    },
+    mainCalculateIndividuals : function(){
+        var listOfWords = this.calculateAllDistances();
+        this.calculateMaxDistances(listOfWords);
+        var listPercentagePerDistance = this.calculatePercentagePerDistance(listOfWords);
+        var listOfIndividuals = this.calculateAllWeight(listOfWords,listPercentagePerDistance);
+        var result= "";
+        listOfIndividuals.sort(function compare(indivudalA,individualB){
+            if (indivudalA.getDistance() < individualB.getDistance())
+                return 1;
+            if (indivudalA.getDistance() > individualB.getDistance())
+                return -1;
+            return 0;
+            }
+        );
+
+        for(var index = 0 ; index<listOfIndividuals.length ; index++){
+            result+=listOfIndividuals[index].getWordString() + " distancia : " + 
+                    listOfIndividuals[index].getDistance() + " peso: " + 
+                    listOfIndividuals[index].getWeigth() + " //// ";
+        }
+        alert(result);
+        return listOfIndividuals;
     }
 
 });
